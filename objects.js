e285a0e62215f8188d967a64752d25ff795eb398
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-16 00:01:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,11 @@ playlist = {
 }
 
 function updatePlaylist(playlist, artist, song) {
-  playlist.artist = song;
+  playlist.artist = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
   return playlist
 }
